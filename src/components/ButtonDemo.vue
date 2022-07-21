4b6411190987e0fa1bf3cea8fe145d8668949d4d
HHuangYQ <1135782479@qq.com>
2022-07-21 22:04:51 +0800

    创建Buttons组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
-       button文档   
+        button 示例
     </div>
+    <h1>示例1</h1>
+    <Buttons>你好</Buttons>
 </template>
 
-
 <script lang="ts">
+import Buttons from '../lib/Buttons.vue'
 export default{
-
+    components:{Buttons}
 }
 </script>
