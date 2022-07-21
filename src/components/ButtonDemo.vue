7204a53ecace683b403e02937a3bcccf07954a29
HHuangYQ <1135782479@qq.com>
2022-07-21 22:31:16 +0800

    添加theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,10 +2,11 @@
     <div>button 示例</div>
     <h1>示例1</h1>
     <div>
-        <Buttons @click="onClick" 
-        @focus="onClick" 
-        @mouseover="onClick"
-        >你好</Buttons>
+        <Buttons>你好</Buttons>
+        <Buttons theme="button">你好</Buttons>
+        <Buttons theme="link">你好</Buttons>
+        <Buttons theme="text">你好</Buttons>
+
     </div>
 
 </template>
--- src/lib/Buttons.vue
+++ src/lib/Buttons.vue
@@ -1,21 +1,18 @@
     <template>
-    <div :size="size">
-        <button v-bind = "rest">
+        <button class="gulu-button"
+        :class="{[`theme-${theme}`]:theme}">
             <slot />
         </button>
-    </div>
 </template>
 
  
 <script lang="ts">
 export default {
-    inheritAttrs: false,
     props:{
-        
-    },
-    setup(props,context){
-        const {size, ...rest} = context.attrs
-        return{size,rest}
+        theme:{
+            type:String,
+            default:'button'
+        }
     }
 }
 </script>
