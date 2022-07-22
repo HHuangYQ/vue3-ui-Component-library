dece6367c85deb7d0009119b8819c4f37c42b788
HHuangYQ <1135782479@qq.com>
2022-07-22 21:08:00 +0800

    Dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        dialog文档
+    <div>dialog文档</div>
         <h1>示例1</h1>
-        <Dialog/>
-    </div>
+        <Buttons @click="toggle">toggle</Buttons>
+        <Dialog v-model:visible="x" :closeOnClickOverlay="false"
+        :ok="f1" :cancel="f2"
+        />
 </template>
 
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Buttons from '../lib/Buttons.vue'
+import { ref } from 'vue'
 export default{
-    components:{Dialog}
+    components:{ Dialog, Buttons },
+    setup(){
+        const x = ref (false)
+        const toggle = ()=>{
+            x.value = !x.value
+        }
+        const f1 = ()=>{
+            return false
+        }
+        const f2 =()=>{}
+        return {x , toggle,f1,f2 }
+    }
 }
 </script>
