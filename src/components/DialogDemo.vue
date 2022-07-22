acef3769aa035db2f2cc346b416df54fecb4e355
HHuangYQ <1135782479@qq.com>
2022-07-22 20:15:29 +0800

    创建dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,12 +1,15 @@
 <template>
     <div>
-        diglog文档
+        dialog文档
+        <h1>示例1</h1>
+        <Dialog/>
     </div>
 </template>
 
 
 <script lang="ts">
+import Dialog from '../lib/Dialog.vue'
 export default{
-
+    components:{Dialog}
 }
 </script>
