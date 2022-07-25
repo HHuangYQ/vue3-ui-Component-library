7ab1376cfc099443ed0b254b1f368ed65e4bac54
HHuangYQ <1135782479@qq.com>
2022-07-25 19:10:38 +0800

    实现下划线滑动

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -1,23 +1,28 @@
 <template>
-    <div>Tabs示例</div>
-    <h1>示例一</h1>
-    <Tabs v-model:selected="x">
-        <Tab title="导航1">内容1</Tab>
-        <Tab title="导航2">内容2</Tab>
-    </Tabs>
+<div>Tabs 示例</div>
+<h1>示例1</h1>
+<Tabs v-model:selected="x">
+  <Tab title="导航1">内容1</Tab>
+  <Tab title="导航2">内容2</Tab>
+</Tabs>
 </template>
 
-
 <script lang="ts">
-import Tabs from "../lib/Tabs.vue"
-import Tab from "../lib/Tab.vue"
-import { ref } from "vue"
-
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+import {
+  ref
+} from 'vue'
 export default {
-    components: { Tab, Tabs },
-    setup(){
-        const x =ref('导航1')
-        return {x}
+  components: {
+    Tabs,
+    Tab
+  },
+  setup() {
+    const x = ref('导航1')
+    return {
+      x
     }
+  }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="gulu-tabs">
-        <div class="gulu-tabs-nav">
+        <div class="gulu-tabs-nav" ref="container">
             <div class="gulu-tabs-nav-item" :class="{ selected: t === selected }" v-for="(t, index) in titles"
-                @click="select(t)" :key="index">{{ t }}</div>
+                :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :key="index">{{ t }}</div>
+            <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="gulu-tabs-content">
-            <component class="gulu-tabs-content-item" :is="current" />
+            <component class="gulu-tabs-content-item" :class="{ selected: c.props.title === selected }"
+                v-for="c in defaults" :is="c" />
         </div>
     </div>
 </template>
 <script lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted, onUpdated, ref } from 'vue'
 import Tab from './Tab.vue'
 export default {
     props: {
@@ -19,6 +21,30 @@ export default {
         }
     },
     setup(props, context) {
+        const navItems = ref<HTMLDivElement[]>([])
+        const indicator = ref<HTMLDivElement>(null)
+        const container = ref<HTMLDivElement>(null)
+        const x = (()=>{
+            const divs = navItems.value
+            const result = divs.filter(div => div.classList.contains('selected'))[0]
+            const { width } = result.getBoundingClientRect()
+            indicator.value.style.width = width + 'px'
+            const { left:left1 } = container.value. getBoundingClientRect()
+            const {left:left2} = result.getBoundingClientRect()
+            const left = left2 - left1
+            indicator.value.style.left = left + 'px '
+        })
+        onMounted(() => {
+            const divs = navItems.value
+            const result = divs.filter(div => div.classList.contains('selected'))[0]
+            const { width } = result.getBoundingClientRect()
+            indicator.value.style.width = width + 'px'
+            const { left:left1 } = container.value. getBoundingClientRect()
+            const {left:left2} = result.getBoundingClientRect()
+            const left = left2 - left1
+            indicator.value.style.left = left + 'px '
+        })
+        onUpdated(x)
         const defaults = context.slots.default()
         defaults.forEach((tag) => {
             if (tag.type !== Tab) {
@@ -36,7 +62,7 @@ export default {
         const select = (title: string) => {
             context.emit('update:selected', title)
         }
-        return { defaults, titles, current, select }
+        return { defaults, titles, current, select, navItems, indicator, container }
     }
 }
 
@@ -52,6 +78,8 @@ $border-color: #d9d9d9;
         display: flex;
         color: $color;
         border-bottom: 1px solid $border-color;
+        position: relative;
+
 
         &-item {
             padding: 8px 0;
@@ -66,10 +94,29 @@ $border-color: #d9d9d9;
                 color: $blue;
             }
         }
+
+        &-indicator {
+            position: absolute;
+            height: 3px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            width: 100px;
+            transition: all 250ms;
+        }
     }
 
     &-content {
         padding: 8px 0;
+
+        &-item {
+            display: none;
+
+            &.selected {
+                display: block;
+            }
+        }
+
     }
 }
 </style>
