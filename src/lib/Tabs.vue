4f32c7c161e3b697610ce915e15ca8f435bc3f9d
HHuangYQ <1135782479@qq.com>
2022-07-25 19:19:27 +0800

    优化1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,7 +2,7 @@
     <div class="gulu-tabs">
         <div class="gulu-tabs-nav" ref="container">
             <div class="gulu-tabs-nav-item" :class="{ selected: t === selected }" v-for="(t, index) in titles"
-                :ref="el => { if (el) navItems[index] = el }" @click="select(t)" :key="index">{{ t }}</div>
+                :ref="el => { if (t===selected) selectedItem = el }" @click="select(t)" :key="index">{{ t }}</div>
             <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
         </div>
         <div class="gulu-tabs-content">
@@ -21,16 +21,14 @@ export default {
         }
     },
     setup(props, context) {
-        const navItems = ref<HTMLDivElement[]>([])
+        const selectedItem = ref<HTMLDivElement>(null)
         const indicator = ref<HTMLDivElement>(null)
         const container = ref<HTMLDivElement>(null)
         const x = (() => {
-            const divs = navItems.value
-            const result = divs.filter(div => div.classList.contains('selected'))[0]
-            const { width } = result.getBoundingClientRect()
+            const { width } = selectedItem.value.getBoundingClientRect()
             indicator.value.style.width = width + 'px'
             const { left: left1 } = container.value.getBoundingClientRect()
-            const { left: left2 } = result.getBoundingClientRect()
+            const { left: left2 } = selectedItem.value.getBoundingClientRect()
             const left = left2 - left1
             indicator.value.style.left = left + 'px '
         })
@@ -55,7 +53,7 @@ export default {
         const select = (title: string) => {
             context.emit('update:selected', title)
         }
-        return { defaults, titles, current, select, navItems, indicator, container }
+        return { defaults, titles, current, select,selectedItem, indicator, container }
     }
 }
 
