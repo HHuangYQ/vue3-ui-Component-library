9e4f88e1510a03bd33f3d675ef5353abe93f3caf
HHuangYQ <1135782479@qq.com>
2022-07-26 19:27:07 +0800

    复杂一点的高亮

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,20 +1,20 @@
 <template>
     <div class="layout">
-        <Topnav toggleMenuButtonVisible class="nav"/>
+        <Topnav toggleMenuButtonVisible class="nav" />
         <div class="content">
             <aside v-if="menuVisible">
-            <h2>文档</h2>
-            <ol>
-                <li>
-                    <router-link to="/doc/intro">介绍</router-link>
-                </li>
-                 <li>
-                    <router-link to="/doc/install">安装</router-link>
-                </li>
-                 <li>
-                    <router-link to="/doc/get-started">开始使用</router-link>
-                </li>
-            </ol>
+                <h2>文档</h2>
+                <ol>
+                    <li>
+                        <router-link to="/doc/intro">介绍</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/install">安装</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/get-started">开始使用</router-link>
+                    </li>
+                </ol>
                 <h2>组件列表</h2>
                 <ol>
                     <li>
@@ -32,7 +32,7 @@
                 </ol>
             </aside>
             <main>
-                <router-view/>
+                <router-view />
             </main>
         </div>
     </div>
@@ -46,64 +46,78 @@ export default {
     components: {
         Topnav
     },
-     setup(){
+    setup() {
         const menuVisible = inject<Ref<boolean>>('menuVisible')
-        return {menuVisible}
+        return { menuVisible }
     }
 }
 </script>
 <style lang="scss" scoped>
-//高亮选择路由
-.router-link-active{
-    text-decoration: underline;
-}
 .layout {
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  > .nav {
-    flex-shrink: 0;
-  }
-  > .content {
-    flex-grow: 1;
-    padding-top: 60px;
-    padding-left: 156px;
-    @media (max-width: 500px) {
-      padding-left: 0; 
-    }
-  }
-}
-.content {
-  display: flex;
-  > aside {
-    flex-shrink: 0;
-  }
-  > main {
-    flex-grow: 1;
-    padding: 16px;
-    background: white;
-  }
-}
-aside{
-    background: lightblue;
-    width: 150px;
-    padding: 16px;
-    position: fixed;
-  top: 0;
-  left: 0;
-  padding-top: 70px;
-  height: 100%;
-    
-    >h2{
-        margin-bottom: 4px;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
 
+    >.nav {
+        flex-shrink: 0;
     }
-    >ol{
-        >li{
-            padding: 4px 0 ;
+
+    >.content {
+        flex-grow: 1;
+        padding-top: 60px;
+        padding-left: 156px;
+
+        @media (max-width: 500px) {
+            padding-left: 0;
         }
     }
-    main{
+}
+
+.content {
+    display: flex;
+
+    >aside {
+        flex-shrink: 0;
+    }
+
+    >main {
+        flex-grow: 1;
+        padding: 16px;
+        background: white;
+    }
+}
+
+aside {
+    background: lightblue;
+    width: 150px;
+    padding: 16px 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    padding-top: 70px;
+    height: 100%;
+
+    >h2 {
+        margin-bottom: 4px;
+    padding: 0 16px;
+    }
+
+    >ol {
+        >li {
+            >a {
+                padding: 4px 16px;
+                display: block;
+                text-decoration: none;
+            }
+
+            //高亮选择路由
+            .router-link-active {
+                background: white;
+            }
+        }
+    }
+
+    main {
         overflow: auto;
     }
 }
