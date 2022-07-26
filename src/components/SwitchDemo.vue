36e6d257f1659b73e1fba41f7704dc0a3fcd5d6a
HHuangYQ <1135782479@qq.com>
2022-07-26 22:17:29 +0800

    删掉无用代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,55 +1,55 @@
 <template>
     <div>
-  <h1>Switch 组件示例 </h1>
-  <Demo :component="Switch1Demo"/>
-  <Demo :component="Switch2Demo"/>
+        <h1>Switch 组件示例 </h1>
+        <Demo :component="Switch1Demo" />
+        <Demo :component="Switch2Demo" />
 
-</div>
+    </div>
 </template>
 
 
 <script lang="ts">
-import Buttons from '../lib/Buttons.vue'
-import { ref } from 'vue'
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
-import 'prismjs'
-import 'prismjs/themes/prism.css'
 import Demo from './Demo.vue'
-const Prism = (window as any).Prism
-export default{
-    components:{ Buttons, Demo },
-    setup(){
-        const bool = ref(false)
-        return{bool,Switch1Demo,Switch2Demo,Prism }
+export default {
+    components: { Demo },
+    setup() {
+        return { Switch1Demo, Switch2Demo }
     }
 }
 </script>
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
+
 .demo {
-  border: 1px solid $border-color;
-  margin: 16px 0 32px;
-  >h2 {
-    font-size: 20px;
-    padding: 8px 16px;
-    border-bottom: 1px solid $border-color;
-  }
-  &-component {
-    padding: 16px;
-  }
-  &-actions {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-  }
-  &-code {
-    padding: 8px 16px;
-    border-top: 1px dashed $border-color;
-    >pre {
-      line-height: 1.1;
-      font-family: Consolas, 'Courier New', Courier, monospace;
-      margin: 0;
+    border: 1px solid $border-color;
+    margin: 16px 0 32px;
+
+    >h2 {
+        font-size: 20px;
+        padding: 8px 16px;
+        border-bottom: 1px solid $border-color;
     }
-  }
-} 
+
+    &-component {
+        padding: 16px;
+    }
+
+    &-actions {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+    }
+
+    &-code {
+        padding: 8px 16px;
+        border-top: 1px dashed $border-color;
+
+        >pre {
+            line-height: 1.1;
+            font-family: Consolas, 'Courier New', Courier, monospace;
+            margin: 0;
+        }
+    }
+}
 </style>
