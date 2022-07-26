9fab1a22f751d32907ba2df7b8d776213df28e91
HHuangYQ <1135782479@qq.com>
2022-07-26 19:35:33 +0800

    引入github-markdown-css，填充文档内容

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { createApp } from 'vue'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import './lib/svg.js'
-
 import './index.scss'
+import 'github-markdown-css'
 
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
