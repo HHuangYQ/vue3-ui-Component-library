e1c8450962a2a1c8fde05bf8400ccb1d45f34009
HHuangYQ <1135782479@qq.com>
2022-07-28 18:37:38 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import './lib/gulu.scss'
 import { router } from './router';
 import { createApp } from 'vue'
-// import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import App from './App.vue'
 import './assets/svg.js'
 import './index.scss'
@@ -9,9 +8,6 @@ import 'github-markdown-css'
 import Markdown from './components/Markdown.vue'
 
 const app = createApp(App)
-// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-//     app.component(key, component)
-  // }
 app.use(router)
 app.mount('#app')
 app.component("Markdown",Markdown)
