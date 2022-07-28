a8f21818a5af162cd23183564e50e1e13e042244
HHuangYQ <1135782479@qq.com>
2022-07-28 16:31:45 +0800

    添加自动部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-rm -rf dist 
-yarn build
-cd dist
-git init
-git add .
-git commit -m 'update'
-git brsnch -M main
-git remote add origin git@github.com:HHuangYQ/Grape-ui.git
-git push -f -u origin main
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'update' &&
+git branch -M main &&
+git remote add origin https://github.com/HHuangYQ/Grape-ui &&
+git push -f -u origin main &&
 cd -
+echo hhuangyq.github.io/grape-ui/index.html
